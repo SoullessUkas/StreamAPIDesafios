9113d9f71642c05f8771061fd7ce7d94b6ad9dc2
Lucas Almeida <lucasmirandaalmeida2@gmail.com>
2024-10-09 17:25:57 -0300

    Completa desafio 1,2,3

---
+++ src/Desafios/DesafioDois.java
@@ -0,0 +1,20 @@
+package Desafios;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class DesafioDois {
+public static void main(String[] args) {
+    List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+    int somaTotalDosPares = numeros.stream()
+    .filter(n->n%2==0)
+    .mapToInt(Integer::intValue)
+    .sum();
+    System.out.println("Soma Toatal: "  + somaTotalDosPares);
+    
+    
+    
+}
+}
